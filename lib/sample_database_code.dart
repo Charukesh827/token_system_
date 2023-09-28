40bcaa8f43db900dadb12a72431c67480c440473
Sanjeeth-18 <sanjeethqwerty@gmail.com>
2023-09-28 20:44:05 +0530

    dbms_added

--- lib/sample_database_code.dart
+++ lib/sample_database_code.dart
@@ -1,56 +1,135 @@
-/*class DatabaseHelper extends ChangeNotifier{
-  static const _databaseName = "token.db";
-  static const _databaseVersion = 1;
-  static const table='token';
-  int eggState=0;
-  int vegState=0;
-  int nonVegState=0;
+import 'dart:async';
+import 'package:sqflite/sqflite.dart' as sql; //sqflite package
+class TOK {
+  Future<sql.Database> start() async{
+    return sql.openDatabase(
+      "tokkens.db",
+      version:1,
+      onCreate : (sql.Database db,int version) async{
+        await create(db);
+      });
+  }
 
-  late Database _db;
+  Future create(sql.Database db) async {
+    await db.execute("""
+      CREATE TABLE student(
+                    roll TEXT PRIMARY KEY ,
+                    sname TEXT,
+                    course TEXT,
+                    dob TEXT,
+                    yoj TEXT,
+                    pass TEXT
+                    )"""
+    );
 
-  Future<void> init()async{
-    final documentsDirectory = await  getApplicationDocumentsDirectory();
-    final path = join(documentsDirectory.path , _databaseName);
-    _db = await openDatabase(
-      path,
-      version: _databaseVersion,
-      onCreate: _onCreate,
+    await db.execute("""
+        CREATE TABLE token (
+                  roll TEXT ,
+                  date TEXT,
+                  veg INTEGER,
+                  non INTEGER,
+                  egg INTEGER,   //doubt
+                  CONSTRAINT f_key FOREIGN KEY (roll) REFERENCES login(roll)
+                  )"""
+    );
+    await db.execute("""
+      CREATE TABLE employee(
+                    empid TEXT PRIMARY KEY ,
+                    ename TEXT,
+                    dob TEXT,
+                    doj TEXT,
+                    password TEXT
+                    )"""
+    );
+
+    await db.execute("""
+      create table eggs(
+                  roll TEXT PRIMARY KEY,
+                  count INTEGER
+      )"""
+    );
+
+    await db.execute("""
+      create table management(
+                  id TEXT PRIMARY KEY,
+                  pass TEXT
+    )"""
     );
   }
 
-  Future _onCreate(Database db, int version) async{
-    await db.execute("""CREATE TABLE $table (
-        ROLL VARCHAR(7) PRIMARY KEY,
-        VEG INTEGER,
-        NONVEG INTEGER,
-        EGG INTEGER
-        )""");
+   Future<void> ins_student(String num,String password) async {                                 //insert into student table
+     final db = await start();
+     var data = { 'roll': num, 'pass': password};
+     await db.insert('student', data, conflictAlgorithm: sql.ConflictAlgorithm.replace);
+   }
+
+   Future<void> egg(String num,int e) async{                                                    //insert into egg table
+      final db=await start();
+      var data={'roll' : num,'count' : e};
+      await db.insert('eggs',data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
+   }
+
+
+  Future<void> instoken(String num,int v,int nv,int e) async{                                  //insert into tokken table
+    final db = await start();
+    var datas = { 'roll': num,'veg' : v , 'non' : nv};
+    await db.insert('token', datas,conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    if (e!=0){
+      egg(num,e);
+    }
   }
 
-  Future<void> insert(String roll,int veg,int nonveg, int egg)async{
-    _db.execute("INSERT INTO TABLE VALUES ?,?,?,?",[roll,veg,nonveg,egg]);
+  Future<void> insemployee(String num,String password) async{                                  //insert into employee table
+    final db = await start();
+    var data = { 'empid' : num , 'pass' : password };
+    await db.insert('employee', data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
   }
 
-  Future<void> updateVeg(String roll,int veg)async{
-    _db.execute("UPDATE TOKEN SET VEG=? WHERE ROLL=?",[veg,roll]);
-    vegState=veg;
-    notifyListeners();
+  Future<void> ins_manage(String num,String pass) async{                                       //insert into management table
+    final db=await start();
+    var data={'id' : num , 'pass' : pass};
+    await db.insert('management',data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
   }
 
-  Future<void> updateNonVeg(String roll,int nonVeg)async{
-    _db.execute("UPDATE TOKEN SET NONVEG=? WHERE ROLL=?",[nonVeg,roll]);
-    nonVegState=nonVeg;
-    notifyListeners();
+  Future<void> ins_MtoS(String num,String course,String name,String dob,String doj) async{          //inserting the student basic details by management
+    final db=await start();
+    var data={'roll' : num , 'sname' : name , 'course' : course , 'dob' : dob,'yoj': doj};
+    await db.insert('student',data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
   }
 
-  Future<void> updateEgg(String roll, int egg)async{
-    _db.execute("UPDATE TOKEN SET egg=(SELECT EGG FROM TOKEN WHERE ROLL=?)+? WHERE ROLL=?",[roll,egg,roll]);
-    eggState=egg;
-    notifyListeners();
+  Future<void> ins_MtoE(String id,String name,String dob,String doj) async{          //inserting the employee basic details by management
+    final db=await start();
+    var data={'empid' : id , 'ename' : name , 'dob' : dob,'doj': doj};
+    await db.insert('student',data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
   }
 
-  Future<void>? getVeg(String roll){
-    vegState=_db.execute("SELECT VEG FROM TOKEN WHERE ROLL=?",[roll]) as int;
+  Future<void> update_token(String num , int v, int nv,int e) async{                          //updating the token table
+    final db = await start();
+    var data={'veg' : v , 'non' : nv };
+    await db.update('data',data,where:"num=?",whereArgs:[num]);
+    if (e!=0){
+      egg(num,e);
+    }
+  }
+
+  Future<List<Map<String,dynamic>>> read(String num) async {                                   //displaying
+    final db=await start();
+    return db.query('data',where:"num=?",whereArgs:[num]);
+  }
+
+  Future<void> del_MtoS(String num) async {                                                   //deleting student data by management
+    final db=await start();
+    await db.delete('student',where : "roll=?",whereArgs: [num]);
+  }
+
+  Future<void> del_MtoE(String num) async {                                                  //deleting student data by management
+    final db=await start();
+    await db.delete('employee',where : "empid=?",whereArgs: [num]);
+  }
+
+  Future<void> check_stud() async{
+    final db=await start();
 
   }
-}*/
+}
+
